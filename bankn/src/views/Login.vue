22005878c2ef15e5ea4294383c42bf0296420f3b
Le Huu Nghi <leehuunghi@gmail.com>
2018-12-27 23:14:02 +0700

    add view login

---
+++ bankn/src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="background-image: url('/imgs/bg.png');background-color: #cccccc;">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: "home",
+  components: {
+
+  }
+};
+</script>
