5bf6f3e3c0501028c51c09029308e93846474efe
DESKTOP-4H2CDN2\Admin <trannha132@gmail.com>
2018-12-27 23:16:19 +0700

    refactor router

--- bankn/src/App.vue
+++ bankn/src/App.vue
@@ -1,17 +1,12 @@
 <template>
   <div id="app">
-    <NavBarUser/>
     <router-view/>
   </div>
 </template>
 
 <script>
-import NavBarUser from "@/components/NavBarUser.vue";
 
 export default {
-  components:{
-        NavBarUser
-    }
 }
 </script>
 
