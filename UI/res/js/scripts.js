195edfacea7a492e8e25d72a9253be479500ed29
donguyen131197 <37667792+donguyen131197@users.noreply.github.com>
2019-01-07 05:56:00 +0700

    Card format

--- UI/res/js/scripts.js
+++ UI/res/js/scripts.js
@@ -1,8 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     $(".rep-item").each(function () {
         letter = $(this).find(".rep-name").html().slice(0, 1);
         $(this).find(".rep-ava").html(letter);
     })
-    
+
+    $(".card-num-format").each(function () {
+        var foo = $(this).text().split("-").join(""); // remove hyphens
+        if (foo.length > 0) {
+            foo = foo.match(new RegExp('.{1,4}', 'g')).join(" ");
+        }
+        $(this).text(foo);
+    })
+
 })
